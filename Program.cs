b0461ee8129f3112f16170125a42fa6acbccdbb3
perlavaldovinos <perlavaldovinos@PERLAVALDOV6228>
2024-01-24 23:13:48 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class DiceSimulator
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+
+        Console.Write("How many dice rolls would you like to simulate? ");
+        int numberOfRolls = int.Parse(Console.ReadLine());
+
+        DiceRoller diceRoller = new DiceRoller();
+        int[] results = diceRoller.SimulateRolls(numberOfRolls);
+
+        Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        Console.WriteLine($"Total number of rolls = {numberOfRolls}.");
+
+        //this is a for loop to print
+        for (int i = 2; i <= 12; i++)
+        {
+            int percentage = results[i] * 100 / numberOfRolls;
+            string asterisks = new string('*', percentage);
+            Console.WriteLine($"{i}: {asterisks}");
+        }
+
+        Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+    }
+}
+
+class DiceRoller
+{
+    private Random random;
+
+    public DiceRoller()
+    {
+        random = new Random();
+    }
+
+    public int[] SimulateRolls(int numberOfRolls)
+    {
+        int[] results = new int[13]; //stors all possible combinations between the 2 dies
+
+        for (int i = 0; i < numberOfRolls; i++)
+        {
+            int dice1 = random.Next(1, 7);
+            int dice2 = random.Next(1, 7);
+            int sum = dice1 + dice2;
+
+            results[sum]++;
+        }
+
+        return results;
+    }
+}
